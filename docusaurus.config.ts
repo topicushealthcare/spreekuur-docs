91b94dc4d893167acffdcd10f3f65e9b92fb4112
Jan Verhoeckx <janverhoeckx@gmail.com>
2025-08-26 16:14:28 +0200

    rename notice to notification

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -98,9 +98,9 @@ const config: Config = {
                         route: '/openapi/patient-spreekuur',
                     },
                     {
-                        spec: 'openapi/spreekuur/notice-spreekuur.yaml',
-                        id: 'notice-spreekuur',
-                        route: '/openapi/notice-spreekuur',
+                        spec: 'openapi/spreekuur/notification-spreekuur.yaml',
+                        id: 'notification-spreekuur',
+                        route: '/openapi/notification-spreekuur',
                     },
                 ],
                 // Theme Options for modifying how redoc renders them
